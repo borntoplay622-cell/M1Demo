5a28dd906c4c432d59b9e653417a5741c6e78976
Vikram <vikram@gmail.com>
2025-09-23 14:59:55 +0530

    TEST1_Commit

--- src/test/java/com/Demo/NewAddition.java
+++ src/test/java/com/Demo/NewAddition.java
@@ -3,7 +3,7 @@ package com.Demo;
 public class NewAddition {
 
 	public static void main(String[] args) {
-		System.out.println("Newly added changes to Git repo ");
+		System.out.println("Newly added changes TESTING 123");
 
 	}
 
