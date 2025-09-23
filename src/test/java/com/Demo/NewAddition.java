789e25c58e200a87dc2e2beebc66d2be4d0b2ec9
borntoplay622-cell <borntoplay622@gmail.com>
2025-09-23 14:37:02 +0530

    Update NewAddition.java

--- src/test/java/com/Demo/NewAddition.java
+++ src/test/java/com/Demo/NewAddition.java
@@ -3,7 +3,7 @@ package com.Demo;
 public class NewAddition {
 
 	public static void main(String[] args) {
-		System.out.println("Newly added changes ");
+		System.out.println("Newly added changes to Git repo ");
 
 	}
 
