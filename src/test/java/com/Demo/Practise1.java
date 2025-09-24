a61424f2b63d4d62bd612fdf4e4350e75e268f83
vikram <vikram@fe80:0:0:0:b08d:f2ff:fefa:341c%13>
2025-09-24 08:51:56 +0530

    push 1122

--- src/test/java/com/Demo/Practise1.java
+++ src/test/java/com/Demo/Practise1.java
@@ -4,7 +4,7 @@ public class Practise1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("Practise 112");
+ System.out.println("Practise 100");
 	}
 
 }
